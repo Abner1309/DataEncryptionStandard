a847f935ef91ef631b36b38fc14a5929cf6b2a2f
Abner1309 <soul_abner@hotmail.com>
2026-04-22 15:22:18 -0300

    refactor: utilities update

--- src/utilities.c
+++ src/utilities.c
@@ -3,11 +3,14 @@
 
 void free_vector(int* vector) {
     free(vector);
+    vector = NULL;
 }
 
 void free_matrix(int** matrix, int rows) {
     for (int i = 0; i < rows; i++) {
         free(matrix[i]);
+        matrix[i] = NULL;
     }
     free(matrix);
+    matrix = NULL;
 }
