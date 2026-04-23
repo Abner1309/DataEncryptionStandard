f1cfc492d1d4a85c342a0f5a642781d625565642
Abner1309 <soul_abner@hotmail.com>
2026-04-23 11:56:03 -0300

    refactor: utilities update

--- include/utilities.h
+++ include/utilities.h
@@ -3,5 +3,7 @@
 
 void free_vector(int* vector);
 void free_matrix(int** matrix, int rows);
+void print_vector(int* vector, int size);
+void print_matrix(int** matrix, int rows, int columns);
 
 #endif //DATAENCRYPTIONSTANDARD_UTILITIES_H
--- src/utilities.c
+++ src/utilities.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "utilities.h"
 
+#include <stdio.h>
+
 void free_vector(int* vector) {
     free(vector);
     vector = NULL;
@@ -13,4 +15,21 @@ void free_matrix(int** matrix, int rows) {
     }
     free(matrix);
     matrix = NULL;
+}
+
+void print_vector(int* vector, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d", vector[i]);
+    }
+    printf("\n");
+}
+
+void print_matrix(int** matrix, int rows, int columns) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            printf("%d", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
